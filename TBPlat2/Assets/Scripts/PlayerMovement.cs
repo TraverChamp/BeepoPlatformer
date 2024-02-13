1f091062b243e7189a918578c1c7d29eb4c5864b
Traver <traver.blumenfeld@mymail.champlain.edu>
2024-02-13 15:03:27 -0500

    First commit, including capsule player and his movement and ground checks. As well as a good stage

---
+++ TBPlat2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float xSpeed = 10f;
+    [SerializeField] private float groundCheckRadius = 0.1f;
+    [SerializeField] private float jumpForce = 800f;
+    [SerializeField] private LayerMask groundLayer;
+    private Rigidbody2D _rb;
+    private float _xMoveInput;
+    private bool shouldJump;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        _xMoveInput = Input.GetAxis("Horizontal") * xSpeed;
+        if (Input.GetKeyDown(KeyCode.Space)) {
+        shouldJump = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        Collider2D col = Physics2D.OverlapCircle(transform.position, groundCheckRadius, groundLayer);
+        _rb.velocity = new Vector2(_xMoveInput, _rb.velocity.y);
+        bool isGrounded = col != null;
+        if (shouldJump )
+        {
+            if (isGrounded)
+            {
+                _rb.AddForce(Vector2.up * jumpForce);
+                shouldJump = false;
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, groundCheckRadius);
+    }
+}
