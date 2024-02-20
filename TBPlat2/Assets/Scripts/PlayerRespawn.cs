ef8bf77947742db22a076347fca6e41d146b3b46
Traver <traver.blumenfeld@mymail.champlain.edu>
2024-02-20 12:35:22 -0500

    Started work on getting respawns to work. This is infuriating

---
+++ TBPlat2/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    [SerializeField] private Transform respawn;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Hazard"))
+        {
+            transform.position = respawn.transform.position;
+        }
+    }
+}
